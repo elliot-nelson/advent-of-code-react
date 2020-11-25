973b2bdbca4c69bf8ede89c6f128be275f5409d9
Elliot Nelson <nelson.work@gmail.com>
2020-11-25 14:59:23 -0500

    :sparkles: introduce font awesome icons, react style

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
-import React from "react"
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>
+      <div>Hello world!</div>
+      <FontAwesomeIcon icon="space-shuttle" />
+      <FontAwesomeIcon icon="satellite" />
+      <FontAwesomeIcon icon="wrench" />
+      <FontAwesomeIcon icon="ethernet" />
+      <FontAwesomeIcon icon="network-wired" />
+    </div>
 }
