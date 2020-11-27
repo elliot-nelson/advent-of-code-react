e2e6f0fd8b7b9821bfc5142cac30662379353588
Elliot Nelson <nelson.work@gmail.com>
2020-11-27 13:22:58 -0500

    :bug: basic index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,9 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react';
+import { Link } from 'gatsby';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default function Home() {
+export default function Page() {
   return <div>
-      <div>Hello world!</div>
-      <FontAwesomeIcon icon="space-shuttle" />
-      <FontAwesomeIcon icon="satellite" />
-      <FontAwesomeIcon icon="wrench" />
-      <FontAwesomeIcon icon="ethernet" />
-      <FontAwesomeIcon icon="network-wired" />
-      <FontAwesomeIcon icon="network-wired" />
-    </div>
+    <Link to="/2019/01/"><FontAwesomeIcon icon="star" /> The Tyranny of the Rocket Equation (2019 Day 1)</Link>
+    </div>;
 }
