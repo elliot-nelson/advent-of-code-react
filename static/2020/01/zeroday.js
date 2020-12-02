fa1956134af535529f4a0569c9ebfe2a341582af
Elliot Nelson <nelson.work@gmail.com>
2020-12-02 00:38:05 -0500

    :star: 2020 Day 2 zero-day

--- static/2020/01/zeroday.js
+++ static/2020/01/zeroday.js
@@ -22,7 +22,74 @@ async function puzzle(input) {
     }
 }
 
-async function puzzle(input) {
+// 513462
+async function puzzlePart2(input) {
+    let comps = 0;
+    input = input.map(x => parseInt(x, 10));
+    for (let i = 0; i < input.length; i++) {
+        for (let j = i + 1; j < input.length; j++) {
+            for (let k = j + 1; k < input.length; k++) {
+                if (input[i] + input[j] + input[k] === 2020) {
+                    console.log('Part 2 =>', input[i] * input[j] * input[k]) ;
+                    console.log(comps);
+                    return;
+                }
+                comps++;
+            }
+        }
+    }
+}
+
+// 178
+async function puzzlePart2(input) {
+    let comps = 0;
+    input = input.map(x => parseInt(x, 10));
+    input.sort((a,b) => a-b);
+    for (let i = 0; i < input.length; i++) {
+        for (let j = i + 1; j < input.length; j++) {
+            for (let k = j + 1; k < input.length; k++) {
+                if (input[i] + input[j] + input[k] === 2020) {
+                    console.log('Part 2 =>', input[i] * input[j] * input[k]) ;
+                    console.log(comps);
+                    return;
+                }
+                if (input[i] + input[j] + input[k] > 2020) break;
+                comps++;
+            }
+        }
+    }
+}
+
+// 1067
+async function puzzlePart2(input) {
+    let comps = 0;
+    input = input.map(x => parseInt(x, 10));
+    input.sort((a,b) => a-b);
+    for (let i = 0; i < input.length; i++) {
+        for (let j = i + 1; j < input.length; j++) {
+            let bottom = j + 1, top = input.length - 1, k;
+
+            for (;;) {
+                k = Math.floor((bottom + top) / 2);
+                console.log([i, j, bottom, k, top]);
+                if (input[i] + input[j] + input[k] === 2020) {
+                    console.log('Part 2 =>', input[i] * input[j] * input[k]) ;
+                    console.log(comps);
+                    return;
+                } else if (top <= bottom) {
+                    break;
+                } else if (input[i] + input[j] + input[k] < 2020) {
+                    bottom = k + 1;
+                } else {
+                    top = k - 1;
+                }
+                comps++;
+            }
+        }
+    }
+}
+
+async function puzzleX(input) {
     input = input.map(x => parseInt(x, 10));
     input.sort((a,b) => b-a);
     console.log(input);
@@ -48,3 +115,4 @@ async function puzzle(input) {
 }
 
 puzzle(getPuzzleInput(process.argv[2]));
+puzzlePart2(getPuzzleInput(process.argv[2]));
