41391be50b7b6dda4854173344e8a4525d3224c0
Elliot Nelson <nelson.work@gmail.com>
2020-12-02 11:52:38 -0500

    :construction: experimental support for twitter cards

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,6 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
   plugins: [
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
--- src/components/puzzle-page.js
+++ src/components/puzzle-page.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Layout from './layout';
 
 export default function PuzzlePage({ year, day, title, children }) {
   return <Layout>
+    <Helmet>
+      <title>{year} Day {day} - {title}</title>
+      <meta name="author" content="Elliot Nelson" />
+      <meta name="description" content="Solutions to AdventOfCode puzzles, animated in React." />
+      <meta property="og:title" content={`${year} Day ${day} - ${title}`} />
+      <meta property="og:description" content="Solutions to AdventOfCode puzzles, animated in React." />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:image" content={`https://advent-of-code.7tonshark.com/${year}/${day}/card.png` } />
+    </Helmet>
     <header>
       <a href="">All</a> <FontAwesomeIcon icon="angle-right" /> <a href={`/${year}/`}>{year}</a> <FontAwesomeIcon icon="angle-right" /> Day {day} <FontAwesomeIcon icon="star" className="gold-star" /> {title}
     </header>
