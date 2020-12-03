03b6a090ac1723e9b1356c0a593f91d7a3682c45
Elliot Nelson <nelson.work@gmail.com>
2020-12-03 16:26:16 -0500

    :star: 2020 Day 3 part 2 in react

--- src/pages/2020/03/index.js
+++ src/pages/2020/03/index.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { Flipper, Flipped } from 'react-flip-toolkit';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PuzzlePage from '../../../components/puzzle-page';
 import { getPuzzleInput, loadImage, resizeCanvas } from '../../../utils/utils';
 import './index.css';
@@ -57,7 +58,7 @@ class Puzzle extends Component {
       passwords: [],
       search1: [],
       search2: [],
-      trees: 0,
+      part1: 0,
       frame: 0
     };
   }
@@ -65,9 +66,8 @@ class Puzzle extends Component {
   componentDidMount() {
     this.canvas = this.canvasRef.current;
     this.ctx = this.canvas.getContext('2d');
-    this.canvas.width = 400;
-    this.canvas.height = 400;
-    console.log(this.canvas.clientWidth);
+    this.frame = 0;
+    this.camera = { x: 0, y: 0 };
     this.run();
   }
 
@@ -76,11 +76,11 @@ class Puzzle extends Component {
       <div className="puzzle-2020-03">
         <canvas ref={this.canvasRef}></canvas>
         <div className="floater">
-          {this.state.trees &&
-          <div className="solution part1">{this.state.trees} trees</div>
+          {this.state.part1 &&
+          <div className="solution part1"><FontAwesomeIcon icon="tree" /> {this.state.part1}</div>
           }
-          {this.state.trees &&
-          <div className="solution part2">{this.state.trees} trees</div>
+          {this.state.part2 &&
+          <div className="solution part2"><FontAwesomeIcon icon="calculator" /> {this.state.part2}</div>
           }
         </div>
       </div>
@@ -90,14 +90,17 @@ class Puzzle extends Component {
   async run() {
     let input = await getPuzzleInput('input.txt');
     this.map = input.map(line => line.split(''));
-    this.frame = 0;
-    this.solvePart1 = solve(this.map, { x: 0, y: 0 }, { x: 3, y: 1 }, 4 * 60);
 
     this.images = await Promise.all([
       loadImage('map1.png'),
       loadImage('map2.png'),
-      loadImage('map3.png')
+      loadImage('map3.png'),
+      loadImage('map4.png')
     ]);
+
+    this.solvePart1 = solve(this.map, { x: 0, y: 0 }, { x: 3, y: 1 });
+    this.wait = 15;
+    this.pos = [{ x: 0, y: 0 }];
 
     Object.assign(this, resizeCanvas(this.canvas, this.ctx, 480, 270, true, true));
     window.requestAnimationFrame(t => this.update(t));
@@ -110,16 +113,49 @@ class Puzzle extends Component {
     this.lastTime = t;
     this.frame++;
 
-    let result = this.solvePart1.next();
-    if (result.value) {
-      this.pos = result.value.pos;
-      this.collisions = result.value.collisions;
-      this.setState({ trees: result.value.trees });
-      console.log(this.trees);
+    if (this.wait && this.wait > 0) {
+      this.wait--;
+    } else if (this.solvePart1) {
+      let result = this.solvePart1.next();
+      if (result.value) {
+        this.pos = [result.value.pos];
+        this.collisions = [result.value.collisions];
+        this.setState({ part1: result.value.trees });
+      }
+      if (result.done) {
+        this.solvePart1 = undefined;
+        this.solvePart2 = true;
+        this.wait = 45;
+      }
+    } else if (this.solvePart2 === true) {
+      this.solvePart2 = [
+        solve(this.map, { x: 0, y: 0 }, { x: 1, y: 1 }),
+        solve(this.map, { x: 0, y: 0 }, { x: 3, y: 1 }),
+        solve(this.map, { x: 0, y: 0 }, { x: 5, y: 1 }),
+        solve(this.map, { x: 0, y: 0 }, { x: 7, y: 1 }),
+        solve(this.map, { x: 0, y: 0 }, { x: 1, y: 2 })
+      ];
+      this.pos = [{ x: 0, y: 0 }, { x: 0, y: 0 }, { x: 0, y: 0 }, { x: 0, y: 0 }, { x: 0, y: 0 }];
+      this.collisions = [{}, {}, {}, {}, {}];
+      this.trees = [0, 0, 0, 0, 0];
+    } else if (this.solvePart2) {
+      let done = false;
+      for (let i = 0; i < this.solvePart2.length; i++) {
+        let result = this.solvePart2[i].next();
+        if (result.value) {
+          this.pos[i] = result.value.pos;
+          this.collisions[i] = result.value.collisions;
+          this.trees[i] = result.value.trees;
+        }
+        done = done && result.done;
+      }
+      if (done) {
+        this.solvePart2 = undefined;
+      }
+      this.setState({ part2: this.trees.reduce((a, value) => a * value) });
     }
 
     Object.assign(this, resizeCanvas(this.canvas, this.ctx, 480, 270, true));
-
     this.draw();
 
     window.requestAnimationFrame(t => this.update(t));
@@ -131,52 +167,53 @@ class Puzzle extends Component {
     this.ctx.fillStyle = 'black';
     this.ctx.fillRect(0, 0, this.width, this.height);
 
+    if (!this.pos || !this.collisions) return;
+
+    let focus = this.pos[(this.frame / 45 | 0) % this.pos.length];
+
+    this.camera.x = this.camera.x * 0.7 + focus.x * 0.3;
+    this.camera.y = this.camera.y * 0.7 + focus.y * 0.3;
+
     let offset = {
-      x: this.width / 2 - this.pos.x * 8,
-      y: this.height / 2 - this.pos.y * 8
+      x: this.width / 2 - this.camera.x * 8,
+      y: this.height / 2 - this.camera.y * 8
     };
 
     this.ctx.translate(offset.x, offset.y);
 
-    if (this.pos) {
-      let sWidth = Math.ceil(this.width / 2 / 8),
-          sHeight = Math.ceil(this.height / 2 / 8);
-      for (let y = this.pos.y - sHeight | 0; y < this.pos.y + sHeight | 0; y++) {
-        if (y >= 0 && y < this.map.length) {
-          for (let x = this.pos.x - sWidth | 0; x < this.pos.x + sWidth | 0; x++) {
-            this.ctx.drawImage(this.images[0], x * 8 - 4, y * 8 - 7);
-          }
+    let sWidth = Math.ceil(this.width / 2 / 8),
+        sHeight = Math.ceil(this.height / 2 / 8);
+    for (let y = this.camera.y - sHeight | 0; y < this.camera.y + sHeight | 0; y++) {
+      if (y >= 0 && y < this.map.length) {
+        for (let x = this.camera.x - sWidth | 0; x < this.camera.x + sWidth | 0; x++) {
+          this.ctx.drawImage(this.images[0], x * 8 - 4, y * 8 - 7);
         }
       }
+    }
 
+    for (let i = 0; i < this.pos.length; i++) {
       this.ctx.strokeStyle = 'red';
       this.ctx.lineWidth = 2;
+      this.ctx.beginPath();
       this.ctx.moveTo(0, 0);
-      this.ctx.lineTo(this.pos.x * 8, this.pos.y * 8);
+      this.ctx.lineTo(this.pos[i].x * 8, this.pos[i].y * 8);
       this.ctx.stroke();
+      this.ctx.drawImage(this.images[3], this.pos[i].x * 8 - 2, this.pos[i].y * 8 - 6);
+    }
 
-      for (let y = this.pos.y - sHeight | 0; y < this.pos.y + sHeight | 0; y++) {
-        if (y >= 0 && y < this.map.length) {
-          for (let x = this.pos.x - sWidth | 0; x < this.pos.x + sWidth | 0; x++) {
-            let tree = this.map[y][(x + this.map[0].length) % this.map[0].length] === '#';
-            let collide = this.collisions[`${x},${y}`];
-            if (collide) {
-              this.ctx.drawImage(this.images[2], x * 8 - 4, y * 8 - 7);
-            } else if (tree) {
-              this.ctx.drawImage(this.images[1], x * 8 - 4, y * 8 - 7);
-            }
+    for (let y = this.camera.y - sHeight | 0; y < this.camera.y + sHeight | 0; y++) {
+      if (y >= 0 && y < this.map.length) {
+        for (let x = this.camera.x - sWidth | 0; x < this.camera.x + sWidth | 0; x++) {
+          let tree = this.map[y][(x + this.map[0].length) % this.map[0].length] === '#';
+          let collide = this.collisions.find(c => c[`${x},${y}`]);
+          if (collide) {
+            this.ctx.drawImage(this.images[2], x * 8 - 4, y * 8 - 7);
+          } else if (tree) {
+            this.ctx.drawImage(this.images[1], x * 8 - 4, y * 8 - 7);
           }
         }
       }
     }
-      /*this.ctx.strokeStyle = 'red';
-      this.ctx.lineWidth = 2;
-      this.ctx.beginPath();
-      this.ctx.moveTo(0, 0);
-      this.ctx.lineTo(result.value.pos.x, result.value.pos.y);
-      this.ctx.stroke();
-      this.ctx.fillStyle = 'white';
-      this.ctx.fillText(JSON.stringify(result.value.pos) + ", " + this.frame, 10, 90);*/
   }
 }
 
