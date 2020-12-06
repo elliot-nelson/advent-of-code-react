64b754a64b6acee079a54af387876313781457d1
Elliot Nelson <nelson.work@gmail.com>
2020-12-06 12:41:27 -0500

    :sparkles: utility set functions for future puzzles

--- static/util.js
+++ static/util.js
@@ -5,11 +5,29 @@
 // leaderboard for a puzzle they come in handy. (Sometimes, I'll use these in the React
 // solution as well just because I've already written it.)
 
+const fs = require('fs');
+
+function getInput(filename) {
+  let lines = fs.readFileSync(filename, 'utf8').split('\n');
+  if (lines[lines.length - 1] === '') lines.splice(-1);
+  return lines.join('\n');
+}
+
+function parseInt2(value) {
+  return parseInt(value, 2);
+}
+
 function int(value) {
+  return parseInt(value, 10);
+}
+function parseInt10(value) {
   return parseInt(value, 10);
 }
 
 function hex(value) {
+  return parseInt(value, 16);
+}
+function parseInt16(value) {
   return parseInt(value, 16);
 }
 
@@ -32,4 +50,34 @@ function obj(array, keys) {
   return result;
 }
 
-module.exports = { int, hex, scan, obj };
+function union(...arrays) {
+  let a = arrays.shift().concat(...arrays);
+  return [...new Set(a)];
+}
+
+function intersection(...arrays) {
+  let a = arrays.shift(), b;
+  while (b = arrays.shift()) {
+    a = a.filter(x => b.includes(x));
+  }
+  return a;
+}
+
+function difference(...arrays) {
+  let a = arrays.shift(), b;
+  while (b = arrays.shift()) {
+    a = a.filter(x => !b.includes(x));
+  }
+  return a;
+}
+
+module.exports = {
+  getInput,
+  parseInt2,
+  parseInt10,
+  parseInt16,
+  int, hex, scan, obj,
+  union,
+  intersection,
+  difference
+};
